4305c8d23d41a1933efe6d221c7c7ef90abc8483
dhairyagit <130041242+dhairyagit@users.noreply.github.com>
2023-04-09 18:04:01 +0530

    Add files via upload

---
+++ ADD.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num1,num2,result;
+	printf("ENTER THE FIRST NUMBER:");
+	scanf("%d", &num1);
+	printf("ENTER THE SECOND NUMBER:");
+	scanf("%d", &num2);
+	result = num1 + num2;
+	printf("ADDITION OF TWO NUMBER IS %d",result);
+}
